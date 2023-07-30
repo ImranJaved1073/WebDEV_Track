8d02b184aedc62a942bc9dd1a460ff15332ff380
ImranJaved <imranjaved1073@gmail.com>
2023-07-30 09:29:02 +0500

    callenge 2

---
+++ Lesson4/Challenge2.js
@@ -0,0 +1,17 @@
+// 1
+const firstNumber = 20;
+const secondNumber = '20';
+
+const result = firstNumber === secondNumber; // false
+
+console.log(result);
+
+//2
+const firstNumber = 20;
+const secondNumber = '20';
+
+const result = firstNumber == secondNumber; 
+
+console.log(result); //true
+
+// 1 is recommend to use because in this we we more clear about data types
